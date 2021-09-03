572d87d0a5f7a36b8488d0aec33e4fa3c9553ec7
jeffclassy <32966151+jeffclassy@users.noreply.github.com>
2021-09-03 09:44:51 +0800

    Update Main Element Clashes.py

--- Main Element Clashes.py
+++ Main Element Clashes.py
@@ -23,7 +23,7 @@ from Autodesk.Revit.DB import *
 #import asd
 #clr.path.append('C:\Users\jeff.mercado\Downloads')
 #clr.AddReference('BimorphNodes')
-Apath=os.path.normpath('C:\Users\jeff.mercado\Downloads\BimorphNodes.dll')
+Apath=os.path.normpath('C:\Users\jaypm.mercado\Downloads\BimorphNodes.dll')
 clr.AddReference(Apath)
 from Revit import Element as Bimorph#, LinkElement
 from Revit import LinkElement as LinkedBimorph
