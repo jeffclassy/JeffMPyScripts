4afe48aa9057713eed9d81506b1bfea654f53915
jeffclassy <32966151+jeffclassy@users.noreply.github.com>
2021-09-06 17:46:29 +0800

    Update Main Element Clashes.py

--- Main Element Clashes.py
+++ Main Element Clashes.py
@@ -12,10 +12,9 @@ import clr
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
 from Autodesk.DesignScript.Geometry import Curve as GeomCurves
+from Autodesk.DesignScript.Geometry import Solid as Sld
 from Autodesk.DesignScript.Geometry import Point as Pnt
-from Autodesk.DesignScript.Geometry import Vector as Vctr
-
-
+from Autodesk.DesignScript.Geometry import Line as Ln
 
 clr.AddReference('System')
 from System.Collections.Generic import List
@@ -31,10 +30,29 @@ from Autodesk.Revit.DB import *
 clr.AddReference('DSCoreNodes') 
 from DSCore import Web as wb
 from DSCore import String as strng
+from DSCore import List as lst
+#from DSCore import Revit.Elements as Rvt
 
 clr.AddReference('BimorphNodes')
 from Revit import Element as Bimorph#, LinkElement
 from Revit import LinkElement as LinkedBimorph
+import Curve as BMCurve
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+# Start Transaction
+doc = DocumentManager.Instance.CurrentDBDocument
+TransactionManager.Instance.EnsureInTransaction(doc)
+
+curvelistB = List[GeomCurves]()
+origin = Pnt.ByCoordinates(0,0)
+empty=[]
+catA=IN[0][0][1]
+catB=IN[0][0][3]
 
 def verif (d):
 	if d[0] == host and d[1] == hostname and d[2] == chr(49):
@@ -48,6 +66,68 @@ def fixrotation (rot):
 		rot
 	return rot
 
+def clash (arc,mep):
+	try:
+		meploc = mep.Location
+		if isinstance(meploc,GeomCurves):
+			mepgeom = GeomCurves.TrimByEndParameter(meploc,0.999)
+			curvelistB.Add(mepgeom)		
+		else:			
+			mepgeom = Ln.ByStartPointEndPoint(meploc,Pnt.ByCoordinates(meploc.X,meploc.Y,meploc.Z+1))
+			curvelistB.Add(mepgeom)
+		intersect = BMCurve.SolidIntersection(Sld.ByUnion(arc.Solids),curvelistB)["Curve[]"]
+		curvelistB.Clear()	
+	except:
+		intersect = 0
+	return intersect
+		
+def getmidpoint (crv):
+	try:
+		ptoi = GeomCurves.PointAtParameter(lst.FirstItem(crv),0.5)
+		return ptoi #point of intersection
+	except:
+		return crv
+
+def mepparameters (mepelems):
+	if catB == "Ducts":
+		return ductparams(mepelems)
+	elif catB == "Cable Trays":
+		return trayparams(mepelems)
+	elif catB == "Pipes" or catB == "Conduits":
+		return pipeparams(mepelems)
+	else:
+		return ductparams(mepelems)
+ductparameters = ["Width","Height","System Type","Workset"]
+trayparameters = ["Width","Height","Service Type","Workset"]
+pipeparameters = ["Diameter","System Abbreviation","Workset"]
+typeparameters = []
+
+def ductparams (mepelems):
+	dp=[]
+	for p in ductparameters:
+		try:
+			dp.append (LinkedBimorph.GetParameterValueByName(mepelems,p))
+		except:
+			dp.append("")
+	return dp
+def trayparams (mepelems):
+	trayp=[]
+	for p in trayparameters:
+		try:
+			trayp.append (LinkedBimorph.GetParameterValueByName(mepelems,p))
+		except:
+			trayp.append("")
+	return trayp
+
+def pipeparams (mepelems):	
+	pipep=[]
+	for p in pipeparameters:
+		try:
+			pipep.append (LinkedBimorph.GetParameterValueByName(mepelems,p))
+		except:
+			pipep.append("")
+	return pipep
+
 elementsListA = List[Revit.Elements.Element]()
 elementsListB = List[Revit.Elements.Element]()
 
@@ -57,8 +137,8 @@ rvtlinkinstance1=IN[0][0][0]
 rvtlinkinstance2=IN[0][0][2]
 ur1 = '19ZI50dVh_m5Azsb0D_ONsBIhEkziUk9hvK5LzJprY6k/edit?usp=sharing/'
 
-category1=Revit.Elements.Category.ByName(IN[0][0][1])
-category2=Revit.Elements.Category.ByName(IN[0][0][3])
+category1=Revit.Elements.Category.ByName(catA)
+category2=Revit.Elements.Category.ByName(catB)
 arcelems=LinkedBimorph.OfCategory(rvtlinkinstance1,category1)
 mepelems=LinkedBimorph.OfCategory(rvtlinkinstance2,category2)
 url = 'https://docs.google.com/spreadsheets/d/'	
@@ -68,6 +148,9 @@ for a in arcelems:
 	elementsListA.Add(a)
 for m in mepelems:
 	elementsListB.Add(m)
+arcelems.Clear()
+mepelems.Clear()
+
 lc=False
 for line in lctab:
 	if not strng.StartsWith(line,"<!DOCTYPE"):
@@ -79,32 +162,25 @@ for line in lctab:
 		else:
 			if verif(strng.Split(line,",")):
 				lc=True
-bimorphclash = Bimorph.IntersectsElement(elementsListA,elementsListB) if lc else []
-try:
-	for a,m in zip(arcelems,bimorphclash["Element[][]"]):
-		for n in m:
-			try:
-				intersections = Geometry.Intersect(BoundingBox.ToCuboid(a.BoundingBox),n.Location)[0]
-				nunwrap = UnwrapElement(n)
-				ntype = nunwrap.Document.GetElement(nunwrap.GetTypeId())		
-				aunwrap = UnwrapElement(a)
-				atype = aunwrap.Document.GetElement(aunwrap.GetTypeId())	
-				try:
-					pointofintersection = GeomCurves.PointAtParameter(intersections,0.5)
-				except:
-					pointofintersection = intersections
-				try:
-					#rotangle = round(math.degrees(Vctr.AngleAboutAxis(a.Location.Direction,Vctr.XAxis(),Vctr.ZAxis()))%360,1)
-					rotangle = (Vctr.AngleAboutAxis(a.Location.Direction,Vctr.XAxis(),Vctr.ZAxis()))
-				except:
-					try:
-						rotangle = math.degrees((aunwrap.Location).Rotation)
-					except:
-						rotangle = 0
-				rot=round(fixrotation(rotangle),1)
-				out.append([a,n,intersections,atype,ntype,pointofintersection,rot])
-			except:
-				pass
-except:
-	out=[]
+			
+bimorphclash = Bimorph.IntersectsElement(elementsListA,elementsListB)# if 
+arcelems = bimorphclash["intersectsWith[]"]
+mepelems = bimorphclash["Element[][]"]
+intersections=[]
+for a,m in zip(arcelems,mepelems):
+	for n in m:
+		intersections = clash(a,n)
+		if not(intersections == 0 or intersections==[]):
+			pointofintersection = getmidpoint(intersections)
+		else:
+			pointofintersection = intersections
+		if isinstance(pointofintersection,Pnt):
+			nunwrap = UnwrapElement(n)
+			aunwrap = UnwrapElement(a)
+			ntype = nunwrap.Document.GetElement(nunwrap.GetTypeId())		
+			atype = aunwrap.Document.GetElement(aunwrap.GetTypeId())
+			nparams = mepparameters(n)
+			out.append([a,n,aunwrap,ntype,pointofintersection,nparams])
+		#intersections=[]
+TransactionManager.Instance.TransactionTaskDone()
 OUT = out
